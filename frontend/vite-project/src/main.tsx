11288ae6ecee3d1a95674d75506b1f8d08722f36
Ruturaj <kruturaj96@gmail.com>
2026-01-18 20:36:55 +0530

    Added analytics

--- frontend/vite-project/src/main.tsx
+++ frontend/vite-project/src/main.tsx
@@ -2,11 +2,12 @@ import { Provider } from "@/components/ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-
+import { Analytics } from "@vercel/analytics/react"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>,
 )
